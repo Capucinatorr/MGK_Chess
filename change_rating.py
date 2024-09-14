ffba687c47e1b540c9652e34a8e1e0a974db0501
Capucinatorr <mark-litvak@mail.ru>
2024-09-14 18:18:42 +0300

    Added cmd.txt (help file) and functions

--- change_rating.py
+++ change_rating.py
@@ -6,11 +6,17 @@ def load():
     rating.close()
     return table
 
+def load_cmd():
+    cmdf = open("cmd.txt", "r")
+    cmdlist = json.loads(cmdf.read())
+    cmdf.close()
+    return cmdlist
+
 def save(table):
     rating = open("rating.txt", "w")
     rating.write(json.dumps(table))
     rating.close()
-    # print(json.dumps(table, sort_keys=True, indent=4))
+#    print(json.dumps(table, sort_keys=True, indent=4))
     print("Rating updated!")
 
 def add_player(name, games=0, tournaments=0, rating=1200):
@@ -52,6 +58,12 @@ def add_result(w_name, b_name, w_points):
     save(table)
     print("Results added!")
 
+def add_tournament(name):
+    table = load()
+    table[name][2] += 1
+    save(table)
+    print("Tournament added!")
+
 def show_table(table):
     print("{} | {} | {} | {}".format("Фамилия И.".ljust(15, " "), "Игры".rjust(5, " "), "Турн.".rjust(5, " "), "ELO".rjust(4, " ")))
     print("-" * 38)
@@ -59,6 +71,26 @@ def show_table(table):
         print("{} | {} | {} | {}".format(i[1][0].ljust(15, " "), str(i[1][1]).rjust(5, " "), str(i[1][2]).rjust(5, " "), str(i[1][3]).rjust(4, " ")))
         print("=" * 38)
 
-table = load()
-show_table(table)
-add_result("Тест", "Тест 1", 0)
+cmd_list = load_cmd()
+
+while True:
+    table = load()
+    cmd = input("> ")
+    if cmd == "help":
+        print(*sorted(cmd_list.items()), sep="\n")
+    elif cmd == "reg":
+        name = input("Фамилия И.> ")
+        if name not in table:
+            add_player(name)
+        add_tournament(name)
+    elif cmd == "game":
+        wname, bname = input("Игрок белыми> "), input("Игрок черными> ")
+        wscore = float(input("Очки белых (1, 0.5, 0)> "))
+        add_result(wname, bname, wscore)
+    elif cmd == "exit":
+        save(table)
+        break
+    elif cmd == "table":
+        show_table(table)
+    else:
+        print("Type 'help' for command list.")
